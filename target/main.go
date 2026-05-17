eb04ef329f594aac38720283459fd9e63875da10
tyumu <tyumusuke2321@gmail.com>
2026-05-17 12:56:57 +0900

    標的用サーバーをお試しで立ててみた

---
+++ target/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+var hitCount int64
+
+func main(){
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
+		current := atomic.AddInt64(&hitCount, 1)
+		fmt.Fprintf(w, "ヒット数: %d\n", current)
+	})
+	
+	fmt.Println("標的用サーバーが 8080 で起動しました。")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("死にました: %v\n", err)
+	}
+}
