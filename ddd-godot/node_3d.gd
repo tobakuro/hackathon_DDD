846b0f3ea7e8de3e4d305475243f7ce594ba0443
tyumu <115434121+tyumu@users.noreply.github.com>
2026-05-24 08:58:13 +0900

    ゴーファー君をつかめるように

--- ddd-godot/node_3d.gd
+++ ddd-godot/node_3d.gd
@@ -5,6 +5,13 @@ extends XROrigin3D
 @onready var camera: XRCamera3D = $XRCamera3D
 @onready var left_hand: XRController3D = $LeftHand
 @onready var right_hand: XRController3D = $RightHand
+@onready var left_hand_area: Area3D = $LeftHand/Area3D
+@onready var right_hand_area: Area3D = $RightHand/Area3D
+
+var _left_trigger_prev := false
+var _right_trigger_prev := false
+
+
 func _ready():
 	print("1: _ready started")
 
@@ -25,6 +32,7 @@ func _ready():
 
 func _process(delta):
 	var stick := left_hand.get_vector2("primary")
+	_handle_pickup()
 
 	if stick.length() > 0.1:
 		print("left stick: ", stick)
@@ -41,3 +49,41 @@ func _process(delta):
 	var move_dir := right * stick.x + forward * stick.y
 
 	global_position += move_dir * move_speed * delta
+
+
+func _handle_pickup():
+	var left_trigger_now := left_hand.is_button_pressed("trigger_click")
+	if left_trigger_now and not _left_trigger_prev:
+		var left_target := _find_overlapping_gopher(left_hand_area)
+		if left_target != null:
+			_attach_gopher_to_hand(left_hand, left_target)
+
+	var right_trigger_now := right_hand.is_button_pressed("trigger_click")
+	if right_trigger_now and not _right_trigger_prev:
+		var right_target := _find_overlapping_gopher(right_hand_area)
+		if right_target != null:
+			_attach_gopher_to_hand(right_hand, right_target)
+
+	_left_trigger_prev = left_trigger_now
+	_right_trigger_prev = right_trigger_now
+
+
+func _find_overlapping_gopher(hand_area: Area3D) -> StaticBody3D:
+	for body in hand_area.get_overlapping_bodies():
+		if body is StaticBody3D and str(body.name).begins_with("GoGopher") and body.get_parent() != left_hand and body.get_parent() != right_hand:
+			return body
+	return null
+
+
+func _attach_gopher_to_hand(hand: XRController3D, gopher: StaticBody3D):
+	if gopher == null:
+		return
+
+	if gopher.get_parent() == left_hand or gopher.get_parent() == right_hand:
+		return
+
+	var grabbed_global := gopher.global_transform
+	gopher.reparent(hand)
+	gopher.global_transform = grabbed_global
+
+	print("%s picked by: %s" % [gopher.name, hand.name])
